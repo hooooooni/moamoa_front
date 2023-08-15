390c2bca4e9c72c00f34db9a6c08a5c5bc65b323
dhmr11 <jaco4539@naver.com>
2023-08-15 21:48:31 +0900

    Feat:Swiper 기능 구현

---
+++ src/pages/LookforPage.jsx
@@ -0,0 +1,42 @@
+import React,{useState} from 'react'
+import { Content, ContentOuter } from '../components/Styled'
+import NotifiModal from '../Modals/NotifiModal'
+import styled from 'styled-components';
+function LookforPage() {
+  const [notifimodalOpen, setNotifimodalOpen] =useState(false);
+
+  const handleClick =()=>{
+    setNotifimodalOpen(true);
+  }
+
+  
+   const Btn= styled.button`
+  width:90px;
+  height: 30px;
+  border-radius: 5px;
+  font-weight: 600;
+  color:white;
+  background-color: #60A1FF;
+  border:1px solid transparent;
+
+`
+  return (
+    <>
+      <ContentOuter>
+        <Content>
+
+
+          <Btn onClick={handleClick}>둘러보기</Btn> 
+            {notifimodalOpen && (
+            <NotifiModal setNotifimodalOpen={setNotifimodalOpen}/>
+            )}
+
+
+
+        </Content>
+      </ContentOuter>
+    </>
+  )
+}
+
+export default LookforPage
---
+++ src/pages/Myreview.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { Content, ContentOuter } from "../components/Styled";
+import NotifiModal from "../Modals/NotifiModal";
+import styled from "styled-components";
+
+function Myreview() {
+  return (
+    <>
+      <ContentOuter>
+        <Content></Content>
+      </ContentOuter>
+    </>
+  );
+}
+
+export default Myreview;
