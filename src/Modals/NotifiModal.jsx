ff7eb9bf07e91c0dfcdeb3476e512c29518b70f5
dhmr11 <jaco4539@naver.com>
2023-08-17 23:12:04 +0900

    Feat:충돌 해결

--- src/Modals/NotifiModal.jsx
+++ src/Modals/NotifiModal.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import styled from "styled-components";
 import { AiOutlineClose } from "react-icons/ai";
 import { useEffect, useState } from "react";
@@ -39,58 +38,3 @@ export default function NotifiModal({ setNotifimodalOpen }) {
     </>
   );
 }
-=======
-import styled from 'styled-components';
-import { AiOutlineClose } from "react-icons/ai";
-import { useEffect, useState } from "react";
-
-
-export const NotifiContainer = styled.div`
-    position: fixed;
-    /* z-index: 1200; */
-    width: 40%;
-    height: 50%;
-    background-color:transparent;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    
-    background-color: #273345;
-;
-`
-
-export const NotifiModalCloseBtn = styled.button`
-    z-index: 1000;
-    color: white;
-    position: relative;
-    right:-45%;
-    top:5%;
-    font-size:20px;
-    background-color: transparent;
-    border:1px transparent;
-;
-`
-
-
-
-
-export default function NotifiModal({
-    setNotifimodalOpen
-}){
-    return (
-        <>
-            <NotifiContainer>
-                
-                    <NotifiModalCloseBtn onClick={()=> setNotifimodalOpen(false)}>
-                    
-                        X
-                    </NotifiModalCloseBtn>
-                
-            </NotifiContainer>
-            
-        </>
-    )
-    
-    
-}
->>>>>>> 604006f443a6261f8666de33b67882c5967498f9
