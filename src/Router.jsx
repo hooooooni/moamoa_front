415354250f1a9fca5fb81685ffbd170b7656fec3
dhmr11 <jaco4539@naver.com>
2023-08-18 03:52:23 +0900

    camelcase수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,46 +2,39 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import MainPage from "./pages/MainPage";
 import Signup from "./Signup";
-import LookforPage from "./pages/LookforPage";
-import MyReview from "./pages/Myreview";
+import LookForPage from "./pages/LookForPage";
+import MyReview from "./pages/MyReview";
 import StorePage from "./pages/StorePage";
 
-
-
-
 const router = createBrowserRouter([
+  {
+    path: "App",
+    element: <App />,
+    children: [
+      {
+        path: "MainPage",
+        element: <MainPage />,
+      },
 
-    {
-        path: "App",
-        element: <App />,
-        children: [
-            {
-                path: "MainPage",
-                element: <MainPage />
-            },
+      {
+        path: "LookForPage",
+        element: <LookForPage />,
+      },
 
-            {
-                path: "LookforPage",
-                element: <LookforPage />
-            },
-
-            {
-                path: "MyReview",
-                element: <MyReview />
-            },
-            {
-                path: ":Storepage",
-                element: <StorePage />
-            },
-
-        ],
-
-    },
-    {
-        path: "/",
-        element: <Signup />
-
-    },
+      {
+        path: "MyReview",
+        element: <MyReview />,
+      },
+      {
+        path: ":Storepage",
+        element: <StorePage />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <Signup />,
+  },
 ]);
 
 export default router;
